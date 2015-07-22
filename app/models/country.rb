941ce07a1c99f8f00b9c65143814192cdbf8fb5a
Jessica Engel & Erin Collins <student@epicodus.com>
2015-07-22 13:53:20 -0700

    navbar dropdown

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,4 +2,6 @@ class Country < ActiveRecord::Base
   validates :c_name, :presence => true
   validates :continent, :presence => true
   validates :c_description, :presence => true
+
+  has_many :projects
 end
