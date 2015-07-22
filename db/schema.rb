585a9ba7cf8fdc387bcb909c4e186017c1f29fcd
chris & franz <student@epicodus.com>
2015-07-22 14:59:21 -0700

    can now edit projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150722212148) do
-=======
-ActiveRecord::Schema.define(version: 20150722202724) do
->>>>>>> 6e060a8939407b928292bddcd2671b13031c7034
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,10 +45,6 @@ ActiveRecord::Schema.define(version: 20150722202724) do
   create_table "projects_users", force: :cascade do |t|
     t.integer "project_id"
     t.integer "user_id"
-  end
-
-  create_table "registrations", force: :cascade do |t|
-    t.string "card_token"
   end
 
   create_table "users", force: :cascade do |t|
