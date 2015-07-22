0ee909d32573439f464e86f2d5fdcce4e4f89234
Jessica Engel & Erin Collins <student@epicodus.com>
2015-07-22 11:40:25 -0700

    functionality of stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => "pk_test_oxYzMN2bUL9Sy0NdklQkZBaL",
+  :secret_key      => "sk_test_ViF0RaCc94XJOZqdvsnl6AOc"
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
