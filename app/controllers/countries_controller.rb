04e9b37630ab30eb119be701cd58176624fe2d42
chris & franz <student@epicodus.com>
2015-07-22 10:47:39 -0700

    country controller added

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -3,4 +3,48 @@ class CountriesController < ApplicationController
   def index
   end
 
+  def new
+    @country = Country.new
+  end
+
+  def create
+    @country = Country.new(country_params)
+    if @country.save
+      flash[:notice] = "Country saved."
+      redirect_to countries_path
+    else
+      flash[:notice] = "Please try again."
+      render :new
+    end
+  end
+
+  def show
+    @country = Country.find(params[:id])
+  end
+
+  def edit
+      @country = Country.find(params[:id])
+  end
+
+  def update
+    @country = Country.find(params[:id])
+    if @country.update(country_params)
+      flash[:notice] = "Your edits have been saved."
+      redirect_to country_path(@country)
+    else
+      flash[:notice] = "Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @country = Country.find(params[:id])
+    @country.destroy
+    redirect_to countries_path
+  end
+
+  private
+  def country_params
+    params.require(:country).permit(:c_name, :continent, :c_description)
+  end
 end
